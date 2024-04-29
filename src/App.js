8859b70c86f45b69f420acd535dc67a0f4b593d5
Despo-coder <damion.wilson@gmail.com>
2024-04-28 20:53:52 -0400

    reconnecting

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
+                        <p>Hello World</p>
                             /* Add Budget component here */
                         }        
 
